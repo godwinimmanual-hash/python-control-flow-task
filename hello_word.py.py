d725edf230d8d9fdd9a3fdf2b9840fdbc791f02a
godwinimmanual-hash <godwinimmanual@gmail.com>
2026-06-26 00:06:31 +0530

    Add files via upload

---
+++ hello_word.py.py
@@ -0,0 +1,20 @@
+# hello_world.py
+
+# Declaring variables of different data types
+name = "Godwin"
+age = 21
+height = 5.6
+is_student = True
+
+# Printing values
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
+
+# Printing data types
+print("\nData Types:")
+print("Type of name:", type(name))
+print("Type of age:", type(age))
+print("Type of height:", type(height))
+print("Type of is_student:", type(is_student))
